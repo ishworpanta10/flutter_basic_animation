4bea4a1ebbfba2172f1a4d6041c2952337eb8bac
ishworpanta10 <ishworpanta10@gmail.com>
2020-08-31 23:56:53 +0545

    Hero animation done

--- lib/shared/tripList.dart
+++ lib/shared/tripList.dart
@@ -53,9 +53,13 @@ class _TripListState extends State<TripList> {
       ),
       leading: ClipRRect(
         borderRadius: BorderRadius.circular(8.0),
-        child: Image.asset(
-          'images/${trip.img}',
-          height: 50.0,
+        child: Hero(
+          
+          tag: 'location_image${trip.img}',
+          child: Image.asset(
+            'images/${trip.img}',
+            height: 50.0,
+          ),
         ),
       ),
       trailing: Text('\$${trip.price}'),
