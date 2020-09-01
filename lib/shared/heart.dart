9c6a2f66bbcf0ae7a494c3ba1e489ffc52682c19
ishworpanta10 <ishworpanta10@gmail.com>
2020-09-01 13:37:48 +0545

    implicit animation with setstate

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -5,13 +5,36 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _colorTween;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+
+    _colorTween = ColorTween(begin: Colors.grey[400], end: Colors.red)
+        .animate(_controller);
+
+    _controller.forward();
+
+    _controller.addListener(() {
+      setState(() {});
+      print(_controller.value);
+      print(_colorTween.value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return IconButton(
       icon: Icon(
         Icons.favorite,
-        color: Colors.grey[400],
+        color: _colorTween.value,
         size: 30,
       ),
       onPressed: () {},
